08e39ae6035ad6d5e890d7caeaa615692ffaf031
lukasbs <lukaszbojes@gmail.com>
2018-10-03 13:03:19 +0200

    Add a Welcome Page

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,36 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class DemoApplication {
+@RestController
+public class DemoApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.antMatcher("/**")
+			.authorizeRequests()
+			.antMatchers("/", "/login**", "/webjars/**", "/error**")
+			.permitAll()
+			.anyRequest()
+			.authenticated();
+	}
+
 }
